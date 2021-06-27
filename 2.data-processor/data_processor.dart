6b9e5eadcdddab505c2fd9de389484b8baf82564
Atik <shakilatik@gmail.com>
2021-06-28 02:54:11 +0600

    Data processor app completed

--- 2.data-processor/data_processor.dart
+++ 2.data-processor/data_processor.dart
@@ -7,7 +7,26 @@ void main(List<String> arguments) {
   }
   final inputFile = arguments.first;
   final lines = File(inputFile).readAsLinesSync();
+  lines.removeAt(0);
+  var totalDuration = 0.0;
+  final totalDurationByTag = <String, double>{};
   for (var line in lines) {
-    print(line);
+    final values = line.split(',');
+    final durationStr = values[3].replaceAll('"', '');
+    final duration = double.parse(durationStr);
+    final tag = values[5].replaceAll('"', '');
+    final previousTotal = totalDurationByTag[tag];
+    if (previousTotal == null) {
+      totalDurationByTag[tag] = duration;
+    } else {
+      totalDurationByTag[tag] = previousTotal + duration;
+    }
+    totalDuration += duration;
   }
+  for (var entry in totalDurationByTag.entries) {
+    final durationFormatted = entry.value.toStringAsFixed(1);
+    final tag = entry.key == '' ? 'Unallocated' : entry.key;
+    print('$tag: ${durationFormatted}h');
+  }
+  print('Total for all tags: ${totalDuration.toStringAsFixed(1)}h');
 }
